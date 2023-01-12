9235458e11f366229ef1c47399b221a259fd113e
Hüseyin Oğuz ÖZYURT <76048675+hoguzzz@users.noreply.github.com>
2023-01-12 20:47:31 +0300

    Add files via upload

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    public class MyDictionary<T>
+    {
+        T[] items;
+        public MyDictionary()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items;
+            items = new T[items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+            items[items.Length-1] = item;
+        }
+        public void RemoveLastIndex()
+        {
+            T[] tempArray = items;
+            items = new T[items.Length - 1];
+            for (int i = 0; i < items.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+        }
+        public void Sort()
+        {
+            Array.Sort(items);
+        }
+        public void Cw()
+        {
+            foreach (var item in items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public int Count
+        {
+            get { return items.Length; }    
+        }
+
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace MyDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> myDictionary = new MyDictionary<string>();
+            Console.WriteLine(myDictionary.Count);
+            myDictionary.Add("Kütahya");
+            myDictionary.Add("Bursa");
+            myDictionary.Add("Eskişehir");
+            myDictionary.Add("İstanbul");
+            myDictionary.Add("İzmir");
+            Console.WriteLine(myDictionary.Count);
+            myDictionary.Sort();
+            myDictionary.RemoveLastIndex();
+            Console.WriteLine(myDictionary.Count);
+            myDictionary.Cw();
+        }
+    }
+}
