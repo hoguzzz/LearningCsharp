736f1ca3245b88b5cacf04ad49381a3be012c008
Hüseyin Oğuz ÖZYURT <76048675+hoguzzz@users.noreply.github.com>
2023-02-02 17:48:53 +0300

    Add files via upload

---
+++ ReCapProject/Business/Concrete/ListCarManager.cs
@@ -0,0 +1,21 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ListCarManager : IListCarService
+    {
+        public void ListCars(List<Car> cars)
+        {
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car.ModelYear + " Model " + car.CarDescription + " " + car.DailyPrice + " Fiyatındadır.");
+            }
+        }
+    }
+}
