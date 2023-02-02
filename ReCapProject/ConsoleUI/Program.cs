8bfb12fd1c083bd8c4b69a56e5a618ff71ea1696
Hüseyin Oğuz ÖZYURT <76048675+hoguzzz@users.noreply.github.com>
2023-02-02 17:49:35 +0300

    Add files via upload

---
+++ ReCapProject/ConsoleUI/Program.cs
@@ -0,0 +1,17 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            ListCarManager listCarManager = new ListCarManager();
+            listCarManager.ListCars(carManager.GetCars());
+            Console.WriteLine("********Id ile getirilen *********");
+            listCarManager.ListCars(carManager.GetCarsById(5));
+        }
+    }
+}
