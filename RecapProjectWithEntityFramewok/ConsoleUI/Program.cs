3cc04925187adaf758b00bb65f57068a6f721c12
Hüseyin Oğuz ÖZYURT <76048675+hoguzzz@users.noreply.github.com>
2023-02-07 18:27:05 +0300

    Add files via upload

---
+++ RecapProjectWithEntityFramewok/ConsoleUI/Program.cs
@@ -0,0 +1,26 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using System.Drawing;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.CarId + " "+ car.CarDescription);
+            }
+            carManager.AddCar(new Car { CarId = 102, BrandId = 11, ColorId = 7, DailyPrice = 0, CarDescription = "Citroen C-Elysee", ModelYear = "2013" });
+            Console.WriteLine("Eklendikten sonra");
+            foreach (var item in carManager.GetAllByBrandId(11))
+            {
+                Console.WriteLine(item.CarId + " " + item.CarDescription);
+            }
+        }
+    }
+}
